60901a68a86c8afd90f875afba85afe8359114ff
Mariano Arias <marianow2006@yahoo.com.ar>
2020-10-25 22:46:45 -0300

    first commit Ejercicio 2

---
+++ ejercicio2/Logic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    public class Logic
+    {
+        public static void LanzarExcepcion()
+        {
+            throw new Exception();
+        }
+
+        public static void LanzarExcepcionPersonalizada()
+        {
+            throw new CustomException("Lanzo mi Exception");
+        }
+    }
+}
---
+++ ejercicio2/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4cd2dc0b-1a9e-4484-bdee-3e0371f214b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
